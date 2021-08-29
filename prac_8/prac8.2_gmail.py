30e4f356e1d38a3a5cde6879af16b71ae56adaac
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-08-29 10:01:47 +0530

    satisfy pep 8
    #5
    #4

--- prac_8/prac8.2_gmail.py
+++ prac_8/prac8.2_gmail.py
@@ -4,7 +4,7 @@ mail_ad = input("enter your mail address: ")
 p = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
 t = p.fullmatch(mail_ad)
 
-if ( t != None ):
+if t is not None: # pep 8
     print("valid mail address")
 else:
     print("invalid mail address")
--- prac_8/prac_8.1_name.py
+++ prac_8/prac_8.1_name.py
@@ -5,7 +5,7 @@ name=input("Enter your name: ")
 p = re.compile('[a-z]+')
 t=p.fullmatch("zman")
 
-if(t != None):
+if t is not None: # pep 8
     print("[Valid]")
 else:
     print("[Invalid]")
