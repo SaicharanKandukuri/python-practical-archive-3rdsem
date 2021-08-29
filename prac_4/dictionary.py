876ee89c155a3cb0e16db0a7d3ebc91a466c905c
Saikiran voladri <200303124537@paruluniversity.ac.in>
2021-08-29 11:34:03 +0530

    prac_4 done
    practical completed

---
+++ prac_4/dictionary.py
@@ -0,0 +1,11 @@
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'apple', 2: 'and', 3: 'mango'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+ 
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'apple', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
---
+++ prac_4/set.py
@@ -0,0 +1,9 @@
+#SET: set used to store multiple items in single variable
+set = { "abhi","sai","kiran","charan"}
+print(set)
+set.add("subba")
+print(set)
+thisset ={"bala","shashi"}
+set.update(thisset)
+print(set)
+print(len(set))
